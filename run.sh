5fb66318097c870ced99984314977e72b5431529
Michael Schwartz <mykesx@gmail.com>
2020-10-26 14:16:15 -0700

    build scripts

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker run \
+    -d \
+    --rm \
+    --name="icomfort-microservice" \
+    icomfort-microservice
---
+++ shell.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -it --rm --name icomfort-microservice icomfort-microservice /bin/bash
